481ac8eb2c9e14b61221aa1c3828894cb64f85a4
Matt Dworkin <mdworkin21@gmail.com>
2018-05-29 16:44:16 -0400

    guessingGame in progress

--- GuessingGame-Part2/GuessingGame.js
+++ GuessingGame-Part2/GuessingGame.js
@@ -1,6 +1,6 @@
 //Generates random number to be guessed 
 function generateWinningNumber(){
-	let num = Math.floor(Math.random() * (100 - 1 + 1))
+	let num = Math.floor(Math.random() * (500 - 1 + 1))
 
 	if (num === 0 ){
 		return 1
@@ -29,13 +29,21 @@ function shuffle(array){
 }
 
 
- function Game(){
+//hmmmmm, haven't used this....
+function newGame(){
+	return new Game();
+}
+
+
+//Game Object Constructor
+function Game(){
  	this.playersGuess = null
  	this.pastGuesses = []
  	this.winningNumber = generateWinningNumber()
  }
 
 
+//Methods on Game Prototype
 Game.prototype.difference = function() {
 	let difference = this.playersGuess > this.winningNumber ?
 		this.playersGuess - this.winningNumber :
@@ -52,7 +60,7 @@ Game.prototype.isLower = function(){
 Game.prototype.playersGuessSubmission = function(guess){
 	this.playersGuess = guess 
 
-	if (this.playersGuess < 1 || this.playersGuess > 100 || typeof this.playersGuess !== 'number'){
+	if (this.playersGuess < 1 || this.playersGuess > 500 || typeof this.playersGuess !== 'number'){
 		throw 'That is an invalid guess.'
 	} else {
 		return this.checkGuess(guess)
@@ -62,68 +70,116 @@ Game.prototype.playersGuessSubmission = function(guess){
 
 
 //Not totally happy with this, it's functional, but refactor if possible 
-Game.prototype.checkGuess = function(){
+Game.prototype.checkGuess = function() {
 
- //  if (this.pastGuesses.indexOf(this.playersGuess) !== -1) {
-	// return 'You have already guessed that number.'
- //  }
+	if (this.pastGuesses.indexOf(this.playersGuess) !== -1) {
+		return 'You already guessed that one dummy.'
 
-  this.pastGuesses.push(this.playersGuess)
+	}
 
-  if (this.winningNumber === this.playersGuess) {
-  	return 'You Win!'
-  } else if (this.pastGuesses.length === 5) {
-  	return 'You Lose.'
-  } else if (this.difference() <= 10) {
-  	return 'You\'re burning up!'
-  } else if (this.difference() < 25) {
-  	return 'You\'re lukewarm.'
-  } else if (this.difference() < 50) {
-  	return 'You\'re a bit chilly.'
-  } else if (this.difference() < 100) {
-  	return 'You\'re ice cold!'
-  }
+	this.pastGuesses.push(this.playersGuess)
+
+	if (this.winningNumber === this.playersGuess) {
+		$('#hint-btn').prop('disabled', true)
+		return 'You Win! I guess luck really is better than skill....'
+	} else if (this.pastGuesses.length === 5) {
+		return 'You Lose.'
+	} else if (this.difference() <= 10) {
+		return 'You\'re burning up!'
+	} else if (this.difference() < 25) {
+		return 'You\'re lukewarm.'
+	} else if (this.difference() < 50) {
+		return 'You\'re a bit chilly.'
+	} else if (this.difference() <100) {
+		return 'You\'re ice cold!'
+	} else if (this.difference() > 100) {
+		return 'Not even close!'
+	}
+
+}
+
+Game.prototype.provideHint = function(){
+	let count = 0;
+	let winner = this.winningNumber;
+
+	return function(){
+		if (count < 1){
+			count++
+			return shuffle([winner, winner + 3, generateWinningNumber(), winner + 8, generateWinningNumber()]) 
+		} else {
+			return 'No More Hints!'
+		}
+	}	
+	
+};
+
+
+Game.prototype.guessInput = function (game){
+	let playerInput = $('#player-input').val()
+	$('h1').text(game.playersGuessSubmission(parseInt(playerInput)))
+}
+
+Game.prototype.displayPastGuess = function (){
+	let playerInput = $('#player-input').val()
+ 	
+ 	 if (this.pastGuesses.length === 1){		
+	   $(`#guess1`).text((parseInt(playerInput)))
+	} else if (this.pastGuesses.length === 2){
+		$('#guess2').text((parseInt(playerInput)))
+	} else if (this.pastGuesses.length === 3){
+		$('#guess3').text((parseInt(playerInput)))
+	} else if (this.pastGuesses.length === 4){
+		$('#guess4').text((parseInt(playerInput)))
+	} else if (this.pastGuesses.length === 5){
+		$('#guess5').text((parseInt(playerInput)))
+	}
+
+		$('#player-input').val('')	
 
 }
 
 
 
-function newGame(){
-	return new Game()
-}
-
-Game.prototype.provideHint = function(){	
-	return shuffle([this.winningNumber, generateWinningNumber(), generateWinningNumber()]) 
-}
-
-
-
+//JQUERY Stuff
 $(document).ready(function(){
-	let game = new Game()
+	//Variables 
+	let game = new Game();
+	let countHint = game.provideHint();
+	
 
 	console.log(game.winningNumber)
 
-	let playerInput = $('#player-input')
-	// let playerGuess = parseInt(playerInput.val())
-
-
+	//Two events to trigger player submission
+	//(i) Click event
 	$('#submit-btn').on('click', function(){
-		$('h1').text(game.playersGuessSubmission(parseInt(playerInput.val())))
-		 playerInput.val('')
-		 // $('.guessDiv').text(playerInput.val())
-		 console.log(game.pastGuesses)
-	 })
+		game.guessInput(game);
+		game.displayPastGuess();
+		
+	});
 
-	$('#hint-btn').on('click', function(){			
-		$('h1').text(game.provideHint())
-		console.log(count)
+	//(ii) Key Event 
+	$('#player-input').on('keydown', function(e){
+		if (e.which === 13){
+			game.guessInput(game);
+			game.displayPastGuess();
 
+
+			
+		}
 	})
 
-		
+	//Event to trigger hint
+	$('#hint-btn').on('click', function(){			
+		$('h1').text(countHint());
+	});
 
+	//Event to trigger reset
+	$('#reset-btn').on('click', function(){
+		location.reload();
 
-})
+	});
+
+});
 
 
 
